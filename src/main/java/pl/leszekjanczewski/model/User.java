34f87a1f45c904c380837a76dc9cfda6ba04090f
leszekjanczewski <leszek.janczewski@gmail.com>
2019-01-09 21:53:59 +0100

    antity

--- src/main/java/pl/leszekjanczewski/model/User.java
+++ src/main/java/pl/leszekjanczewski/model/User.java
@@ -1,7 +1,77 @@
 package pl.leszekjanczewski.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
-@Entity
+//TODO - persistance
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    Long userId;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column
+    String login;
+
+    @Column
+    String password;
+
+    @Column
+    Boolean shift;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getShift() {
+        return shift;
+    }
+
+    public void setShift(Boolean shift) {
+        this.shift = shift;
+    }
 }
