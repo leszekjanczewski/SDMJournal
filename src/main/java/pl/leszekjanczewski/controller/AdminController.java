b5a3e17d723866f361f3868ac0487ff49a9fef6c
leszekjanczewski <leszek.janczewski@gmail.com>
2019-03-07 21:32:37 +0100

    work with testing controlloer

--- src/main/java/pl/leszekjanczewski/controller/AdminController.java
+++ src/main/java/pl/leszekjanczewski/controller/AdminController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import pl.leszekjanczewski.repository.UserRepo;
 
 @Controller
 @RequestMapping("/admin")
