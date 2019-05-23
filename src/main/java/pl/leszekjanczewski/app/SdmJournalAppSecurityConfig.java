ab470132a35118a2c4e1196487e60dc146f5c968
leszekjanczewski <leszek.janczewski@gmail.com>
2019-05-23 17:34:50 +0200

    start configuration spring security, add dependency and create config

--- src/main/java/pl/leszekjanczewski/app/SdmJournalAppSecurityConfig.java
+++ src/main/java/pl/leszekjanczewski/app/SdmJournalAppSecurityConfig.java
@@ -1,7 +1,29 @@
 package pl.leszekjanczewski.app;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SdmJournalAppSecurityConfig {
+@EnableWebSecurity
+public class SdmJournalAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and().formLogin();
+    }
+
 }
